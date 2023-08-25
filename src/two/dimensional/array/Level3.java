1d55cd10d6d070f7b67b5f0d0a61153781cbb1c0
taeminahn <ahntaemin@naver.com>
2023-08-25 18:52:40 +0900

    230825 study3

---
+++ src/two/dimensional/array/Level3.java
@@ -0,0 +1,16 @@
+package two.dimensional.array;
+
+import java.util.Scanner;
+public class Level3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        char[][] arr = new char[5][15];
+
+        for(int i = 0; i < arr.length; i++){
+            for(int j = 0; j < arr[i].length; j++) {
+
+            }
+        }
+    }
+}
